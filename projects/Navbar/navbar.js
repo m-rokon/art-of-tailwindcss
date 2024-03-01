4879e70ff1bfbffc7d61810ca76007f5496a06b0
Rokon <ratertara.rokon@gmail.com>
2024-03-01 21:37:08 +0600

    class 7 - navbar project fix

--- projects/Navbar/navbar.js
+++ projects/Navbar/navbar.js
@@ -1,5 +1,5 @@
 const nav_btn = document.getElementById("nav-btn");
-const nav_menu = document.getElementById("nav-menu");
+const nav_menu = document.getElementById("mobile-menu");
 
 nav_btn.addEventListener("click", ()=> {
     nav_menu.classList.toggle("hidden")
