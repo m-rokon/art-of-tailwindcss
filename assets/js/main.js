ee047f3fbf18f2d1ad82e592de0ed72e64daa40a
Rokon <ratertara.rokon@gmail.com>
2024-03-27 00:04:21 +0600

    Project - Accordion complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,15 @@ questions.forEach((question) => {
     question.addEventListener("click", (e) => {
         e.preventDefault();
         // console.log(question.classList)
+
+        questions.forEach((item) => {
+            // console.log(item);
+            if(item !== question) {
+                item.classList.remove("active");
+                item.nextElementSibling.style.maxHeight = 0;
+            }
+        });
+
         question.classList.toggle("active");
         const accordionBody = question.nextElementSibling;
 
